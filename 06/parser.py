6706f1006f3f1b6c697ce80ec294f71e13e87894
HanGLLYY <lyhanglovej@gmail.com>
2021-04-29 11:43:22 -0500

    parse command line with three king of types

--- 06/parser.py
+++ 06/parser.py
@@ -1,7 +1,5 @@
-  
 from enum import Enum
 class Parser:
-    
     class CommandType(Enum):
         A_COMMAND = 1
         C_COMMAND = 2
@@ -15,11 +13,22 @@ class Parser:
                 if line and not self.is_comment(line):
                     self.commands.append(line)
                     
-
     def is_comment(self, line):
         return line.startswith('//')
+        
+    def command_type(self, cmd):
+        if cmd.startswith('@'):
+            return Parser.CommandType.A_COMMAND
+        elif cmd.startswith('(') and cmd.endswith(')'):
+            return Parser.CommandType.L_COMMAND
+        else: 
+            return Parser.CommandType.C_COMMAND
     
+
 if __name__ == '__main__':
     parser = Parser('Add.asm')
     print(parser.commands)
     print(list(Parser.CommandType))
+    print(parser.command_type('@x'))
+    print(parser.command_type('(LOOP)'))
+    print(parser.command_type('M=0'))
