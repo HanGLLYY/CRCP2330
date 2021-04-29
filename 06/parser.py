3116f1f86dd070e78984c657f786b4e2909c4d09
HanGLLYY <lyhanglovej@gmail.com>
2021-04-29 11:33:32 -0500

    filter empty lines and comments

--- 06/parser.py
+++ 06/parser.py
@@ -1,11 +1,25 @@
-
+  
+from enum import Enum
 class Parser:
+    
+    class CommandType(Enum):
+        A_COMMAND = 1
+        C_COMMAND = 2
+        L_COMMAND = 3
+    
     def __init__(self, asm):
-        self.lines = []
+        self.commands = []
         with open(asm, 'r') as f:
             for line in f:
-                self.lines.append(line.strip())
+                line = line.strip()
+                if line and not self.is_comment(line):
+                    self.commands.append(line)
+                    
 
+    def is_comment(self, line):
+        return line.startswith('//')
+    
 if __name__ == '__main__':
     parser = Parser('Add.asm')
-    print(parser.lines)
+    print(parser.commands)
+    print(list(Parser.CommandType))
